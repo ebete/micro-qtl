66a5b4fa11afb0a73dc0b6cd62f072634726ec5d
Thom Griffioen <thomgriffioen@gmail.com>
2019-07-03 15:54:16 +0200

    Set work dir to script location and the default thread count to cores available

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-snakemake --use-conda -p --cores 8
+CORES=${1:-$(grep -c ^processor /proc/cpuinfo)}
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd "${dir}"
+
+snakemake --use-conda -p --cores "${CORES}"
